92e3316b42b41978ac5cc178187825c78f3c5c63
Cesar Zavaleta Estrada <zavaletap2210@gmail.com>
2025-03-20 00:52:31 -0600

    Funciones de una clase que se pueden utilizar con el mismo proposito, pero con diferente metodo de resolucion

---
+++ Sobrecarga/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math suma = new Math();
+            Console.WriteLine(suma.Suma(2,2));
+            Console.WriteLine(suma.Suma("5","5"));
+            int[] numeros = new int[] { 10, 5, 2, 3 };
+            Console.WriteLine(suma.Suma(numeros));
+        }
+
+    }
+
+    public class Math
+    {
+        // Sobrecarga de métodos
+        //Función que suma dos números enteros
+        public int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        //Función que suma dos números en formato string
+        public int Suma(string a, string b)
+        {
+            return int.Parse(a) + int.Parse(b);
+        }
+        //Función que suma un arreglo de números
+        public int Suma(int[] numeros)
+        {
+            int resultado = 0;
+            int i = 0;
+            while(i < numeros.Length)
+            {
+                resultado += numeros[i];
+                i++;
+            }
+            return resultado;
+        }
+        
+    }
+}
